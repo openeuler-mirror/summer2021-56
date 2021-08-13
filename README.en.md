7207bec60a7d0c6af970d6157300fbe604a112ff
Mianhan Liu <liumh1@shanghaitech.edu.cn>
2021-08-13 19:47:25 +0800

    mid examine

--- README.en.md
+++ README.en.md
@@ -4,19 +4,26 @@
 https://gitee.com/openeuler-competition/summer-2021/issues/I3EIOX
 
 #### Software Architecture
-Software architecture description
+This tool is used to delete the superfluous header files in the linux kernel.The tool runs in three steps
+
+1. For a specific source file A, check all the header files that it has included;
+2. Check all the functions and macro that A has used , then count the numbers of macros/functions A has used in each header file.If some header files was included but used rarely,than it can be deleted and the the declarations can be moved to the other file. For example,A has included a header file B. B has tons of declarations but only one declaration was used.Then we will consider to delete it.
+
+3. After we have confirm the header file we want to delete(say B).We will check whether we can move the only one declaration(say C) to another file(say D).To achieve the goal,we should check all the files in the linux kernel that has used the C.If D was included by every files who has used C.Then we can move the declaration C from B to D.Moreover,the source file A won't need to include the header file B.Thus we have reduced the superfluous header file.
 
 #### Installation
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
+1.  git clone https://gitee.com/openeuler-competition/summer2021-56.git
+2.  compile the project.cpp code with the command: g++ project.cpp -std=c++17 -o project -g -O2 -fopenmp
 
 #### Instructions
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
+there are three mode for running this tool
+1.  input a path to the source file that you want to analyze,than the tool will draw the include dependency graph for you
+2.  input nothing,then the tool will draw the DAG which indicates all the include relationship in the directory.
+3.  input two path.One is the path to the source file,the other is the one you want to check whether it has been included by the first file.For example,./project a.cpp b.hpp will check whether a.cpp has included b.hpp directly or indirectly
+4.  input a path to the source file and the tool will tell you whether there is a superfluous header file can be deleted(to do)
+5.  input a path to the source file and the tool will return all the functions and macros that it has used(to do)
 
 #### Contribution
 
--- project.cpp
+++ project.cpp
@@ -10,147 +10,199 @@
 #include <array>
 #include <cstdio>
 #include <set>
-typedef struct tree_node
-{
-    std::string filename;
-    std::vector<std::shared_ptr<tree_node> > child;
-    std::shared_ptr<tree_node> father;
+#include <sys/resource.h>
+#include <omp.h>
+#include <map>
+typedef struct tree_node {
+	std::string filename;
+	std::vector<std::shared_ptr<tree_node> > child;
+	std::shared_ptr<tree_node> father;
 } node;
+int count_runtimes = 0;
+std::map<std::string, std::shared_ptr<node> > file_map;
+std::set<std::string> write_set;
 using namespace std::filesystem;
-std::string c_header[] = {"assert.h", "limits.h", "signal.h", "stdlib.h", "ctype.h", "locale.h", "stdarg.h", "string.h", "errno.h", "math.h", "stddef.h", "time.h", "float.h", "setjmp.h", "stdio.h", "iso646.h", "wchar.h", "wctype.h", "complex.h", "inttypes.h", "stdint.h", "tgmath.h", "fenv.h", "stdbool.h", "stdalign.h", "stdatomic.h", "stdnoreturn.h", "threads.h", "uchar.h", "aio.h", "libgen.h", "spawn.h", "sys/time.h", "arpa/inet.h", "limits.h", "stdarg.h", "sys/times.h", "assert.h", "locale.h", "stdbool.h", "sys/types.h", "complex.h", "math.h", "stddef.h", "sys/uio.h", "cpio.h", "monetary.h", "stdint.h", "sys/un.h", "ctype.h", "mqueue.h", "sys/utsname.h", "dirent.h", "ndbm.h", "stdlib.h", "sys/wait.h", "dlfcn.h", "net/if.h", "string.h", "syslog.h", "errno.h", "netdb.h", "strings.h", "tar.h", "fcntl.h", "netinet/in.h", "stropts.h", "termios.h", "fenv.h", "netinet/tcp.h", "sys/ipc.h", "tgmath.h", "float.h", "nl_types.h", "sys/mman.h", "time.h", "fmtmsg.h", "poll.h", "sys/msg.h", "trace.h", "fnmatch.h", "pthread.h", "sys/resource.h", "ulimit.h", "ftw.h", "pwd.h", "sys/select.h", "unistd.h", "glob.h", "regex.h", "sys/sem.h", "utime.h", "grp.h", "sched.h", "sys/shm.h", "utmpx.h", "iconv.h", "search.h", "sys/socket.h", "wchar.h", "inttypes.h", "semaphore.h", "sys/stat.h", "wctype.h", "iso646.h", "setjmp.h", "sys/statvfs.h", "wordexp.h", "langinfo.h", "signal.h"};
+std::string c_header[] = {
+	"assert.h",    "limits.h",	"signal.h",	  "stdlib.h",
+	"ctype.h",     "locale.h",	"stdarg.h",	  "string.h",
+	"errno.h",     "math.h",	"stddef.h",	  "time.h",
+	"float.h",     "setjmp.h",	"stdio.h",	  "iso646.h",
+	"wchar.h",     "wctype.h",	"complex.h",	  "inttypes.h",
+	"stdint.h",    "tgmath.h",	"fenv.h",	  "stdbool.h",
+	"stdalign.h",  "stdatomic.h",	"stdnoreturn.h",  "threads.h",
+	"uchar.h",     "aio.h",		"libgen.h",	  "spawn.h",
+	"sys/time.h",  "arpa/inet.h",	"limits.h",	  "stdarg.h",
+	"sys/times.h", "assert.h",	"locale.h",	  "stdbool.h",
+	"sys/types.h", "complex.h",	"math.h",	  "stddef.h",
+	"sys/uio.h",   "cpio.h",	"monetary.h",	  "stdint.h",
+	"sys/un.h",    "ctype.h",	"mqueue.h",	  "sys/utsname.h",
+	"dirent.h",    "ndbm.h",	"stdlib.h",	  "sys/wait.h",
+	"dlfcn.h",     "net/if.h",	"string.h",	  "syslog.h",
+	"errno.h",     "netdb.h",	"strings.h",	  "tar.h",
+	"fcntl.h",     "netinet/in.h",	"stropts.h",	  "termios.h",
+	"fenv.h",      "netinet/tcp.h", "sys/ipc.h",	  "tgmath.h",
+	"float.h",     "nl_types.h",	"sys/mman.h",	  "time.h",
+	"fmtmsg.h",    "poll.h",	"sys/msg.h",	  "trace.h",
+	"fnmatch.h",   "pthread.h",	"sys/resource.h", "ulimit.h",
+	"ftw.h",       "pwd.h",		"sys/select.h",	  "unistd.h",
+	"glob.h",      "regex.h",	"sys/sem.h",	  "utime.h",
+	"grp.h",       "sched.h",	"sys/shm.h",	  "utmpx.h",
+	"iconv.h",     "search.h",	"sys/socket.h",	  "wchar.h",
+	"inttypes.h",  "semaphore.h",	"sys/stat.h",	  "wctype.h",
+	"iso646.h",    "setjmp.h",	"sys/statvfs.h",  "wordexp.h",
+	"langinfo.h",  "signal.h"
+};
+std::string linux_path[] = { "../../include/", "./include/"
+					       "/include/" };
 std::string exec(const char *cmd)
 {
-    std::array<char, 128> buffer;
-    std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if (!pipe)
-    {
-        throw std::runtime_error("popen() failed!");
-    }
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
-    {
-        result += buffer.data();
-    }
-    return result;
+	std::array<char, 128> buffer;
+	std::string result;
+	std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+	if (!pipe) {
+		throw std::runtime_error("popen() failed!");
+	}
+	while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+		result += buffer.data();
+	}
+	return result;
 }
 std::vector<std::string> read_include(std::string path)
 {
-    std::vector<std::string> res;
-    std::ifstream in(path);
-    std::regex include_regex("#include.*");
-    std::string s;
-    if (!in.is_open())
-    {
-        std::cerr << "open file failed" << std::endl;
-    }
-    while (std::getline(in, s))
-    {
-        if (std::regex_match(s, include_regex))
-        {
-            s.erase(remove(s.begin(), s.end(), '\"'), s.end());
-            s.erase(remove(s.begin(), s.end(), '\"'), s.end());
-            s.erase(remove(s.begin(), s.end(), ' '), s.end());
-            s.erase(remove(s.begin(), s.end(), '<'), s.end());
-            s.erase(remove(s.begin(), s.end(), '>'), s.end());
-            s.erase(0, 8);
-            res.push_back(s);
-        }
-    }
-    in.close();
-    return res;
+	std::vector<std::string> res;
+	if (count_runtimes++ >= 20000)
+		return res;
+	std::regex linuix_file_regex("[a-z]+/.*");
+	std::ifstream in(path);
+	std::regex include_regex("#include.*");
+	std::string s;
+	int cnt = 0;
+	while (!in.is_open() && cnt < 14) {
+		in.open(linux_path[cnt++] + path);
+	}
+	if (!in.is_open()) {
+		//std::cerr<<"can't open the file : "<<path<<std::endl;
+	}
+	while (std::getline(in, s)) {
+		s.erase(remove(s.begin(), s.end(), ' '), s.end());
+		if (std::regex_match(s, include_regex)) {
+			s.erase(remove(s.begin(), s.end(), '\"'), s.end());
+			s.erase(remove(s.begin(), s.end(), '\"'), s.end());
+			s.erase(remove(s.begin(), s.end(), '<'), s.end());
+			s.erase(remove(s.begin(), s.end(), '>'), s.end());
+			s.erase(0, 8);
+			res.push_back(s);
+		} else if (s != "" && s[0] != '/' && s[0] != '*' && s[0] != '#')
+			break;
+	}
+	in.close();
+	return res;
 }
 auto list_file(std::string path)
 {
-    std::vector<std::string> res;
-    std::regex valid_filename(".+\\.cpp||.+\\.hpp||.+\\.c||.+\\.h");
-    for (const auto &entry : directory_iterator(path))
-    {
-        std::string s = entry.path().filename().u8string();
-        if (std::regex_match(s, valid_filename)&&s!="project.cpp")
-        {
-            res.push_back(s);
-        }
-    }
-    return res;
+	std::vector<std::string> res;
+	std::regex valid_filename(".+\\.cpp||.+\\.hpp||.+\\.c||.+\\.h");
+	for (const auto &entry : directory_iterator(path)) {
+		std::string s = entry.path().filename().u8string();
+		if (std::regex_match(s, valid_filename) && s != "project.cpp") {
+			res.push_back(s);
+		}
+	}
+	return res;
 }
-std::shared_ptr<node>  tree_create(std::string path, std::shared_ptr<node> parent  = nullptr)
+std::shared_ptr<node> tree_create(std::string path,
+				  std::shared_ptr<node> parent = nullptr)
 {
-    auto root = std::make_shared<node>();
-    root->filename = path;
-    root->father = parent;
-    for (auto &c : read_include(path))
-    {   
-        if(std::find(c_header,c_header+111,c)!=c_header+111){
-            std::shared_ptr<node>  n=std::make_shared<node>();
-            n->filename=c;
-            n->father=root;
-            root->child.push_back(n);
-            continue;
-        }
-        root->child.push_back(tree_create(c, root));
-    }
-    return root;
+	auto root = std::make_shared<node>();
+	if (count_runtimes >= 20000)
+		return root;
+	root->filename = path;
+	root->father = parent;
+	auto vec = read_include(path);
+	int limit = vec.size();
+#pragma omp parallel for
+	for (int i = 0; i < limit; i++) {
+		if (std::find(c_header, c_header + 111, vec[i]) ==
+		    c_header + 111) {
+			auto it = file_map.find(vec[i]);
+			if (it != file_map.end()) {
+				root->child.push_back(it->second);
+				continue;
+			}
+			std::shared_ptr<node> n = std::make_shared<node>();
+			n->filename = vec[i];
+			n->father = root;
+			auto node = tree_create(vec[i], n);
+			root->child.push_back(node);
+			file_map[vec[i]] = node;
+		}
+	}
+	return root;
 }
 void write_image(std::ofstream &out, std::shared_ptr<node> n)
 {
-    for (auto c : n->child)
-    {
-        out << "\"" << n->filename << "\""
-            << " -> "
-            << "\"" << c->filename << "\""
-            << ";" << std::endl;
-        write_image(out, c);
-    }
+	int limit = n->child.size();
+#pragma omp parallel for
+	for (int i = 0; i < limit; i++) {
+#pragma omp critical
+		{
+			out << "\"" << n->filename << "\""
+			    << " -> "
+			    << "\"" << n->child[i]->filename << "\""
+			    << ";" << std::endl;
+		}
+		if (write_set.find(n->child[i]->filename) != write_set.end())
+			continue;
+		write_set.emplace(n->child[i]->filename);
+		write_image(out, n->child[i]);
+	}
 }
-void add_filename(std::set<std::string> &child_file,std::shared_ptr<node> root){
-    for(auto c:root->child){
-        child_file.emplace(c->filename);
-        add_filename(child_file,c);
-    }
+void add_filename(std::set<std::string> &child_file, std::shared_ptr<node> root)
+{
+	for (auto c : root->child) {
+		child_file.emplace(c->filename);
+		add_filename(child_file, c);
+	}
 }
+
 void generate_image(std::shared_ptr<node> root)
 {
-    std::ofstream outfile;
-    outfile.open("./write.dot", std::ios::out);
-    outfile << "digraph {" << std::endl;
-    for (auto c : root->child)
-    {
-        outfile << "\"" << root->filename << "\""
-                << " -> "
-                << "\"" << c->filename << "\""
-                << ";" << std::endl;
-        write_image(outfile, c);
-    }
-    outfile << "}" << std::endl;
-    outfile.close();
-
+	std::ofstream outfile;
+	outfile.open("./write.dot", std::ios::out);
+	outfile << "digraph {" << std::endl;
+	for (auto c : root->child) {
+		outfile << "\"" << root->filename << "\""
+			<< " -> "
+			<< "\"" << c->filename << "\""
+			<< ";" << std::endl;
+		write_image(outfile, c);
+	}
+	outfile << "}" << std::endl;
+	outfile.close();
 }
 int main(int argc, char *argv[])
 {
-    if (argc == 1)
-    {
-        //std::cerr << "fatal error: can't find the input files" << std::endl;
-        auto vec=list_file("./");
-        for(auto c:vec){
-            tree_create(std::string("./")+c);
-        }
-        exec("dot -Tsvg write.dot -o output.svg");
-    }
-    else if (argc == 2)
-    {
-        std::shared_ptr<node> root = tree_create(argv[1]);
-        generate_image(root);
-        exec("dot -Tsvg write.dot -o output.svg");
-    }
-    else if (argc==3){
-        std::shared_ptr<node> root = tree_create(argv[1]);
-        std::set<std::string> child_file;
-        add_filename(child_file,root);
-        if(std::find(child_file.begin(),child_file.end(),argv[2])!=child_file.end()){
-            std::cout<<"already include this file"<<std::endl;
-            return 0;
-        }else{
-            std::cout<<"doesn't include this file"<<std::endl;
-        }
-    }
+	if (argc == 1) {
+		//std::cerr << "fatal error: can't find the input files" << std::endl;
+		auto vec = list_file("./");
+		for (auto c : vec) {
+			tree_create(std::string("./") + c);
+		}
+		exec("dot -Tsvg write.dot -o output.svg");
+	} else if (argc == 2) {
+		std::shared_ptr<node> root = tree_create(argv[1]);
+		generate_image(root);
+		exec("dot -Tsvg write.dot -o output.svg");
+	} else if (argc == 3) {
+		std::shared_ptr<node> root = tree_create(argv[1]);
+		std::set<std::string> child_file;
+		add_filename(child_file, root);
+		if (std::find(child_file.begin(), child_file.end(), argv[2]) !=
+		    child_file.end()) {
+			std::cout << "already include this file" << std::endl;
+			return 0;
+		} else {
+			std::cout << "doesn't include this file" << std::endl;
+		}
+	}
 }
